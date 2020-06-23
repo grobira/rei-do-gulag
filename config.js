cf76a507a36116c07307a409a9131af20f5a92a5
Giovanni Robira <grobira2@gmail.com>
2020-06-23 18:50:38 -0300

    trying to change the api to the offical one

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 const axios = require('axios');
 
 const api = axios.create({
-  baseURL: 'https://call-of-duty-modern-warfare.p.rapidapi.com',
+  baseURL:
+    'https://my.callofduty.com/api/papi-client/stats/cod/v2/title/mw/platform/',
+  //   baseURL: 'https://call-of-duty-modern-warfare.p.rapidapi.com',
   timeout: 10000,
   headers: {
-    'x-rapidapi-host': 'call-of-duty-modern-warfare.p.rapidapi.com',
-    'x-rapidapi-key': process.env.RAPIDAPI_KEY,
+    // 'x-rapidapi-host': 'call-of-duty-modern-warfare.p.rapidapi.com',
+    // 'x-rapidapi-key': process.env.RAPIDAPI_KEY,
     useQueryString: 'true',
   },
 });
