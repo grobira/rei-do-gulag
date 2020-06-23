a265ce710e20d5d71a4b07ddd7a23b8a9b8bbc94
Giovanni Robira <grobira2@gmail.com>
2020-06-23 11:47:07 -0300

    Adding new basic commands

--- commands.js
+++ commands.js
@@ -1,21 +1,111 @@
+const { MessageEmbed } = require('discord.js');
+const { api } = require('./config');
 
+const helpMessage = (command, args, description) => {
+  let embed = new MessageEmbed();
+  embed.setTitle(`${command} Help`);
+  embed.setColor('RED');
+  embed.setDescription(`${description}`);
+  //embed.addField('Args', args, true);
+  return embed;
+};
+
+const commandFactory = (command, args, description, exec) => {
+  return {
+    args,
+    description,
+    exec,
+    help: helpMessage(command, args, description),
+  };
+};
+
+const addPlayer = (commandArgs) => {
+  const [name, tag, type] = commandArgs;
+
+  return `New players added. With ${name}, ${tag}, ${type}`;
+};
+
+const playerStats = async (commandArgs) => {
+  const [name, tag] = commandArgs;
+  let type = 'battle';
+  if (tag === '0') {
+    type = 'psn';
+  }
+  const result = await api.get(`/warzone/${name}%2523${tag}/${type}`);
+  let embed = new MessageEmbed();
+  embed.setTitle(`${name}'s statistics`);
+  embed.setColor('RED');
+  embed.setDescription(`Displaying **${name}**'s Warzone statistics.`);
+  embed.setThumbnail(
+    'https://www.callofduty.com/cdn/app/icons/mw/ranks/mp/icon_rank_155.png'
+  );
+  embed.addField('Lifetime', '\u200b', false);
+  embed.addField(
+    'Kills',
+    result.data.lifetime.mode.br_all.properties.kills,
+    true
+  );
+  embed.addField(
+    'Deaths',
+    result.data.lifetime.mode.br_all.properties.deaths,
+    true
+  );
+  embed.addField(
+    'KDA',
+    result.data.lifetime.mode.br_all.properties.kdRatio.toFixed(2),
+    true
+  );
+  embed.addField('Weekly', '\u200b', false);
+  embed.addField(
+    'Kills',
+    result.data.weekly.mode.br_all.properties.kills,
+    true
+  );
+  embed.addField(
+    'Deaths',
+    result.data.weekly.mode.br_all.properties.deaths,
+    true
+  );
+  embed.addField(
+    'KDA',
+    result.data.weekly.mode.br_all.properties.kdRatio.toFixed(2),
+    true
+  );
+
+  return embed;
+};
 
 const commands = {
-    update(){
-        return "Updating Database"
-    },
-    rank(commandArgs){
-        return "Fetching memers ranks"
-    },
-    addMemer(commandArgs){
-        const [name, tag, type, ] = commandArgs;
-
-        console.log(name, tag, type)
-        return "New memers added"
-    },
-    commandList(){
-        return Object.keys(commands)
-    }
-}
+  update: commandFactory(
+    'update',
+    '',
+    'Update the Database',
+    () => 'Updating database'
+  ),
+  stats: commandFactory(
+    'stats',
+    'name, tag',
+    'Get players stats',
+    (commandArgs) => playerStats(commandArgs)
+  ),
+  rank: commandFactory(
+    'rank',
+    '',
+    'Return the rank of the players',
+    () => 'Fetching players ranks'
+  ),
+  addPlayer: commandFactory(
+    'addPlayer',
+    'name, tag, type',
+    'Add new player to the list',
+    (commandArgs) => addPlayer(commandArgs)
+  ),
+  commandList: commandFactory(
+    'commandList',
+    '',
+    'Get the list of commands avaliable',
+    () => Object.keys(commands)
+  ),
+};
 
 module.exports = commands;
--- index.js
+++ index.js
@@ -1,40 +1,25 @@
 'use strict';
-require('dotenv').config()
+require('dotenv').config();
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-const axios = require("axios");
-const commands = require("./commands");
+const commands = require('./commands');
 const PREFIX = '!';
-;
-
-const api = axios.create({
-    baseURL: 'https://call-of-duty-modern-warfare.p.rapidapi.com',
-    timeout: 10000,
-    headers: {
-    'x-rapidapi-host': 'call-of-duty-modern-warfare.p.rapidapi.com',
-    "x-rapidapi-key": process.env.RAPIDAPI_KEY,
-    "useQueryString": "true",
-    }
-  });
-
 
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-client.on('message', async message => {
-    if (message.content.startsWith(PREFIX)) {
-        const input = message.content.slice(PREFIX.length).split(' ');
-		const command = input.shift();
-        if(commands[command] != null){
-            console.log(input)
-            message.channel.send(commands[command](input));
-        }else{
-            console.log(`Ignoring unknown command`)
-
-        }
+client.on('message', async (message) => {
+  if (message.content.startsWith(PREFIX)) {
+    const input = message.content.slice(PREFIX.length).split(' ');
+    const command = input.shift();
+    if (commands[command] != null) {
+      message.channel.send(await commands[command].exec(input));
+    } else {
+      console.log(`Ignoring unknown command`);
     }
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
